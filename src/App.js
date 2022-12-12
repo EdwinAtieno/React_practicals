7f2aa024002311bdcaf0928cc851cd2487865995
edwinatieno <edwinmuthee11@gmail.com>
2022-12-12 17:41:23 +0300

    feat(click) : handle click in react
    + add click button
    + add click event

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 
 function App() {
-    const title ="React App";
-    const likes = 50;
-    const person = [{name: 'mario', age: 30}, {name: 'luigi', age: 25}, {name: 'yoshi', age: 40}];
   return (
     <div className="App">
       <div className="content">
         <Navbar />
-        <h1>{title}</h1>
           <Home/>
-          <p>Liked {likes} times</p>
-          {person.map((person, index) => (
-          <p>{person?.age}</p>))}
       </div>
     </div>
   );
