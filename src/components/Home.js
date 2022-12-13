4ae47246ab8d0a379d2c4b11767eb0beb94fbb6d
edwinatieno <edwinmuthee11@gmail.com>
2022-12-14 00:27:12 +0300

    feat(message): loading message
    + add loading message while fetching

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
-    fetch(' http://localhost:8000/blogs').then(res =>{
-      return res.json();
-    }).then(data => {
-      console.log(data)
-      setBlogs(data)
-    })
+    setTimeout(() => {
+      fetch(' http://localhost:8000/blogs').then(res => {
+        return res.json();
+      }).then(data => {
+        console.log(data)
+        setIsPending(false)
+        setBlogs(data)
+      });
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
+      {isPending && <div>Loading......</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
