df428c4daec3f6e4f78c2a2381e96dbe42324dab
edwinatieno <edwinmuthee11@gmail.com>
2022-12-13 16:26:32 +0300

    feat(home): use effect
    + add useeffect hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from "./BlogList";
 
 
@@ -13,6 +13,11 @@ const Home = () => {
         const newBlog = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlog)
     }
+
+    useEffect(()=>{
+        console.log("run fast as you can")
+        console.log(blogs)
+    })
     return (
         <div className="home">
             <BlogList blogs={blogs} title="this is the title" handleDelete={handleDelete}/>
