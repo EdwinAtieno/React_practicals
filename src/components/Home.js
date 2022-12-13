677f1592d6d46f9bddfdb698bd505f5d31a2050e
edwinatieno <edwinmuthee11@gmail.com>
2022-12-13 16:39:31 +0300

    feat(useEffect):usesffect dependancy
    + add new useState

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from "./BlogList";
 
 
@@ -8,14 +8,24 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
+    const [name, setName] = useState('mario');
 
     const handleDelete =(id)=>{
         const newBlog = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlog)
     }
+
+    useEffect(()=>{
+        console.log("run fast as you can")
+        console.log(name)
+    }, [name])
+    // empty array makes sure the hook is run only once after the first render
+
     return (
         <div className="home">
             <BlogList blogs={blogs} title="this is the title" handleDelete={handleDelete}/>
+            <button onClick={()=> setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>);
 }
 
